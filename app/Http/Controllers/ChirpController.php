3a344bda2dd6abb3025194edd98d8a15f85ece80
rheam97 <rheam97@gmail.com>
2024-05-09 22:37:18 -0400

    created chrips routes controller and view

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -1,18 +1,24 @@
 <?php
 
+
 namespace App\Http\Controllers;
+
 
 use App\Models\Chirp;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\View\View;
 
 class ChirpController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(): View
     {
         //
+        
+        return view('chirps.index');
     }
 
     /**
